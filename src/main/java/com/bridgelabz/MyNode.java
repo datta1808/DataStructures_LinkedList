7e0b1dea6ff26e1480c3e9fe2d4a03055c4fb20c
datta1808 <bagaledatta18@gmail.com>
2021-06-06 23:04:35 +0530

    ADDED : Created a Simple Linked List

--- src/main/java/com/bridgelabz/MyNode.java
+++ src/main/java/com/bridgelabz/MyNode.java
@@ -1,7 +1,20 @@
 package com.bridgelabz;
 
-public class MyNode {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Linked List Data Structure");
+public class MyNode<T> {
+
+    private T key;
+    private MyNode<T> next;
+
+    public MyNode (T key) {
+        this.key = key;
+        this.next = null;
+    }
+
+    public MyNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode<T> next) {
+        this.next = next;
     }
 }
